49294f2a21403cbb95ad50ce2deb464431932317
Anudip <Anudip@DESKTOP-DEVD6RC>
2022-10-13 05:23:01 -0700

    project to iluustrate control statements

---
+++ ControlStatements/src/com/controlstatemnets/BreakExample1.java
@@ -0,0 +1,22 @@
+package com.controlstatemnets;
+
+public class BreakExample1 {
+
+	public static void main(String[] args) {
+		int i=1;
+		do
+		
+		{
+			if(i==5)
+			{
+				i++;
+				break;
+			}
+			System.out.println(i);
+			i++;
+		}
+		while(i<=10);
+
+	}
+
+}
---
+++ ControlStatements/src/com/controlstatemnets/ContinueExample1.java
@@ -0,0 +1,24 @@
+package com.controlstatemnets;
+
+public class ContinueExample1 {
+
+	public static void main(String[] args) {
+		int i=1;
+		
+		while(i<=10)
+		{
+			if(i==5)
+			{
+				i++;
+				continue;
+			}
+			System.out.println(i);
+			i++;
+		}
+		
+
+	
+
+	}
+
+}
